0f4b25ed2177a3f4f024ca840964b969e40f8b03
Manuel Eduardo Torres Magdaleno <manuel.torres@crediclub.com>
2022-06-04 20:25:15 -0500

    fixed line code

--- src/app/components/list-of-users/list-of-users.component.ts
+++ src/app/components/list-of-users/list-of-users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService } from 'src/app/services/users.service';
-import { Users } from 'src/app/models/users.model';4
+import { Users } from 'src/app/models/users.model';
 import { UserProfile } from 'src/app/models/profile.model';
 import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
