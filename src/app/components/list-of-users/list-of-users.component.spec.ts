4d0dd9e1f987610ee5fb2ee4718ebfc9a6f574f3
Manuel Eduardo Torres Magdaleno <manuel.torres@crediclub.com>
2022-06-07 13:35:20 -0500

    unit test (fix LoadData test)

--- src/app/components/list-of-users/list-of-users.component.spec.ts
+++ src/app/components/list-of-users/list-of-users.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import {InfiniteScrollCustomEvent, IonicModule } from '@ionic/angular';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
@@ -9,6 +9,7 @@ import { ListOfUsersComponent } from './list-of-users.component';
 import { of } from 'rxjs';
 import { UserProfile } from 'src/app/models/Profile.model';
 import { Users } from 'src/app/models/Users.model';
+import { promise } from 'protractor';
 
 describe('ListOfUsersComponent', () => {
   // test Service and test create component
@@ -77,6 +78,7 @@ describe('ListOfUsersComponent', () => {
     expect(spy).toHaveBeenCalled();
   });
 
+
   // test  getUserWithPagination(per_page: number) function
   it('should get users with pagination', () => {
     let response: Users[] = mockUsersArray;
@@ -86,14 +88,14 @@ describe('ListOfUsersComponent', () => {
   });
 
   // test loadData function
-  it('should load data', () => {
-    const event: InfiniteScrollCustomEvent = {} as InfiniteScrollCustomEvent;
-    component.loadData(event);
-    expect(component.loadData).toBeTruthy();
+  it('should call loadData function', fakeAsync(() => {
+    fixture.detectChanges();
+    fixture.componentInstance.loadData(infiniteScroll);
+    tick(500);
+  }));
 
-  });
 
-  // test checkIfDisableInfiniteScroll when users_array.length > 500
+  // test checkIfDisableInfiniteScroll when users_array.length >= 500
   it('should check if disable infinite scroll', () => {
     let lengthTest: number = 500;
     expect(component.checkIfDisableInfiniteScroll(lengthTest)).toBe(true);
